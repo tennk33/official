e0c9d101366c24d5cb5ab240f448c9575b75fadb
github tennk33 <github tennk33@gmail.com>
2021-12-16 10:51:34 +0900

    commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import dog from './images/dog3.png'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>official</h1>
+      <img src={dog} alt="" />
+    </div>
+  );
+}
+
+export default App;
